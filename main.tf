00a28f1c290620632b5344b685834b93ed1329b7
s6peter <s6peter.wft@gmail.com>
2026-09-09 20:57:29 -0500

    feat: update encryption configuration

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = true
 }
